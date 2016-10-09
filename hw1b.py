ac2094e67a17d7ecf2b0cbf6057c759916ed5a3c
ss5136 <ss5136@columbia.ed>
2016-10-09 12:53:58 +0000

    programmingg part added

--- hw1b.py
+++ hw1b.py
@@ -1,5 +1,11 @@
+
+# coding: utf-8
+
+# In[1]:
+
 import os
 from os import walk
+
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
@@ -13,10 +19,7 @@ import theano
 import theano.tensor as T
 
 
-'''
-Implement the functions that were not implemented and complete the
-parts of main according to the instructions in comments.
-'''
+# In[2]:
 
 def reconstructed_image(D,c,num_coeffs,X_mean,im_num):
     '''
@@ -50,11 +53,13 @@ def reconstructed_image(D,c,num_coeffs,X_mean,im_num):
     c_im = c[:num_coeffs,im_num]
     D_im = D[:,:num_coeffs]
     
-    #TODO: Enter code below for reconstructing the image
-    #......................
-    #......................
-    #X_recon_img = ........
-    return X_recon_img
+#     X_recon_img=np.zeros([256,256])
+    temp=np.dot(D_im,c_im).T 
+    temp=temp+X_mean.flatten() 
+    return temp.reshape(256,256)
+
+
+# In[3]:
 
 def plot_reconstructions(D,c,num_coeff_array,X_mean,im_num):
     '''
@@ -90,9 +95,10 @@ def plot_reconstructions(D,c,num_coeff_array,X_mean,im_num):
             
     f.savefig('output/hw1b_{0}.png'.format(im_num))
     plt.close(f)
-    
-    
-    
+
+
+# In[4]:
+
 def plot_top_16(D, sz, imname):
     '''
     Plots the top 16 components from the basis matrix D.
@@ -112,10 +118,24 @@ def plot_top_16(D, sz, imname):
         name of file where image will be saved.
     '''
     #TODO: Obtain top 16 components of D and plot them
-    
+    D_16=D[:,:16]
+    f, axarr = plt.subplots(4,4)
+    k=0
+    for i in range(4):
+        for j in range(4):
+            plt.axes(axarr[i,j])
+            plt.imshow(D_16[:,k].reshape((sz,sz)))
+            k=k+1
+            
+    f.savefig(imname)
+#     print 'output/hw1a_{0}_im{1}.png'.format(n_blocks, im_num)
+    plt.close(f)
+    return
     raise NotImplementedError
 
-    
+
+# In[5]:
+
 def main():
     '''
     Read here all images(grayscale) from Fei_256 folder and collapse 
@@ -123,10 +143,68 @@ def main():
     Make sure the images are read after sorting the filenames
     '''
     #TODO: Write a code snippet that performs as indicated in the above comment
+    for root, dirs, images in os.walk("Fei_256/", topdown=False):
+        pass
+    imageslist=[]
+    imageslist = map(lambda x: os.path.join("Fei_256", x), images)
+    imageslist.sort()
+    im=Image.open(imageslist[0])
+    (width, height) = im.size
+    Ims = np.zeros([len(imageslist), height*width])
+    for i in  range(0, len(imageslist)):
+        im=Image.open(imageslist[i])
+        Ims[i,:]= np.array(im).flatten()
     
     Ims = Ims.astype(np.float32)
     X_mn = np.mean(Ims, 0)
-    X = Ims - np.repeat(X_mn.reshape(1, -1), Ims.shape[0], 0)
+    Images = Ims - np.repeat(X_mn.reshape(1, -1), Ims.shape[0], 0)
+    D=np.random.rand(16,65536)
+    eigen_val=np.zeros(16)
+    for i in range(0,16):
+        learning_para=0.5
+        v=theano.shared(np.random.randn(65536))
+        X=T.matrix()
+#         X_tensor=X
+        Xv=T.dot(X,v)
+#         print Xv.get_value()
+        
+        if i==0:
+            cost=T.dot(Xv.T,Xv)
+            
+        else:
+            
+            cost=T.dot(Xv.T,Xv)-np.sum(eigen_val[j]*T.dot(D[j],v)*T.dot(D[j],v) for j in xrange(i))
+#             cost = T.dot(Xv.T, Xv) - np.sum(evals[j]*T.dot(evecs[j], v)*T.dot(evecs[j], v) for j in xrange(i))
+        
+        gradient=T.grad(cost,v)
+        y=v+learning_para*gradient
+        updated_v_after_descent=y/y.norm(2)
+        final_updated_v=theano.function([X],updates=[(v,updated_v_after_descent)])
+        t=1
+        change =1
+        print "iteration"+str(i)
+        while t<50 and change>0.005:
+            print "T \t"+str(t)
+            print "change \t"+str(change)
+#             set_random(x)
+            final_updated_v(Images)
+            new_di = v.get_value()
+
+            change = np.linalg.norm(D[i]-new_di)
+            D[i]=new_di
+
+
+#             print "Change \t"+str(change)
+            t=t+1
+        tempdot = np.dot(Images, D[i])
+        eigen_val[i] = np.dot(tempdot.T,tempdot)
+        
+
+            
+            
+        
+    D = D.T
+    c = np.dot(D.T, Images.T)
 
     '''
     Use theano to perform gradient descent to get top 16 PCA components of X
@@ -140,12 +218,11 @@ def main():
     #TODO: Write a code snippet that performs as indicated in the above comment
         
     for i in range(0, 200, 10):
-        plot_reconstructions(D=D, c=c, num_coeff_array=[1, 2, 4, 6, 8, 10, 12, 14, 16], X_mean=X_mean.reshape((256, 256)), im_num=i)
+        plot_reconstructions(D=D, c=c, num_coeff_array=[1, 2, 4, 6, 8, 10, 12, 14, 16], X_mean=X_mn.reshape((256, 256)), im_num=i)
 
     plot_top_16(D, 256, 'output/hw1b_top16_256.png')
 
 
 if __name__ == '__main__':
     main()
-    
-    
+
